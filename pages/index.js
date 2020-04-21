391bc79d691e14301d657c727e1b3c89bac3215f
auto200 <michal.warac@gmail.com>
2020-04-21 21:18:39 +0200

    Use material design

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import MaterialUiTheme from "../contexts/MaterialUiTheme";
+import { makeStyles } from "@material-ui/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import {
   TableContainer,
   Table,
@@ -7,6 +10,9 @@ import {
   TableBody,
   TableRow,
   TableCell,
+  Select,
+  Link,
+  Paper,
 } from "@material-ui/core";
 
 const ALL_WORLDS = "ALL_WORLDS";
@@ -24,6 +30,9 @@ const skillTypes = [
   "loyalty",
 ];
 const vocations = ["all", "druid", "knight", "paladin", "sorcerer"];
+
+const useStyles = makeStyles({});
+
 const Index = () => {
   const [worlds, setWorlds] = useState([]);
   const [currentWorld, setCurrentWorld] = useState(ALL_WORLDS);
@@ -31,6 +40,10 @@ const Index = () => {
   const [currentVocation, setCurrentVocation] = useState(vocations[0]);
   const [dataToDisplay, setDataToDisplay] = useState([]);
   const [cache, setCache] = useState({});
+  const classes = useStyles();
+  const pointsNotLevel =
+    currentSkillType === "achievements" || currentSkillType === "loyalty";
+
   useEffect(() => {
     const getWorlds = async () => {
       const { data } = await axios.get(
@@ -54,7 +67,14 @@ const Index = () => {
   useEffect(() => {
     const getHighscores = async () => {
       if (!worlds.length) return;
-
+      const filterId = `${currentWorld}-${currentSkillType}-${currentVocation}`;
+      const cachedData = cache[filterId];
+      if (cachedData) {
+        console.log("data cached!!");
+        setDataToDisplay(cachedData);
+        return;
+      }
+      console.log("downloading new data....");
       if (currentWorld === ALL_WORLDS) {
         console.log(worlds);
         const promises = worlds.map(({ name }) => {
@@ -88,18 +108,37 @@ const Index = () => {
             return 0;
           });
         setDataToDisplay(allServerHighscores);
+        setCache((prev) => {
+          prev[filterId] = allServerHighscores;
+          return prev;
+        });
+      } else {
+        const url = `https://api.tibiadata.com/v2/highscores/${currentWorld}/${currentSkillType}/${currentVocation}.json`;
+        const { data } = await axios.get(url);
+        const highscores = data.highscores.data.map((char) => {
+          char.server = data.highscores.world;
+          return char;
+        });
+        setDataToDisplay(highscores);
+        setCache((prev) => {
+          prev[filterId] = highscores;
+          return prev;
+        });
       }
     };
     getHighscores();
   }, [worlds, currentWorld, currentSkillType, currentVocation]);
   return (
-    <div>
+    <MaterialUiTheme>
+      <CssBaseline />
       <div>
-        <label>
-          World
-          <select
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <Select
+            labelId="World"
             value={currentWorld}
+            native={true}
             onChange={(e) => setCurrentWorld(e.target.value)}
+            key={worlds.length}
           >
             <option value={ALL_WORLDS}>all</option>
             {worlds.map(({ name }) => (
@@ -107,74 +146,73 @@ const Index = () => {
                 {name}
               </option>
             ))}
-          </select>
-        </label>
-      </div>
-      <div>
-        <label>
-          Skill
-          <select
+          </Select>
+          <Select
             value={currentSkillType}
             onChange={(e) => setCurrentSkillType(e.target.value)}
+            labelId="Skill"
+            native={true}
           >
             {skillTypes.map((skill) => (
               <option key={skill} value={skill}>
                 {skill}
               </option>
             ))}
-          </select>
-        </label>
-      </div>
-      <div>
-        <label>
-          Vocation
-          <select
+          </Select>
+          <Select
             value={currentVocation}
             onChange={(e) => setCurrentVocation(e.target.value)}
+            labelId="Vocation"
+            native={true}
           >
             {vocations.map((vocation) => (
               <option key={vocation} value={vocation}>
                 {vocation}
               </option>
             ))}
-          </select>
-        </label>
-      </div>
-      <TableContainer>
-        <Table>
-          <caption>display area</caption>
-          <TableHead>
-            <TableRow selected>
-              <TableCell>Rank</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Level</TableCell>
-              <TableCell>Vocation</TableCell>
-              <TableCell>Server</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {dataToDisplay.slice(0, 300).map((data, i) => (
-              <TableRow key={`${data.name}_${data.level}_${i}`} hover>
-                <TableCell>{i + 1}</TableCell>
-                <TableCell>
-                  <a
-                    href={`https://www.tibia.com/community/?subtopic=characters&name=${encodeURIComponent(
-                      data.name
-                    )}`}
-                    target="_blank"
-                  >
-                    {data.name}
-                  </a>
-                </TableCell>
-                <TableCell>{data.level}</TableCell>
-                <TableCell>{data.voc}</TableCell>
-                <TableCell>{data.server}</TableCell>
+          </Select>
+        </div>
+        <TableContainer component={Paper}>
+          <Table stickyHeader={true}>
+            <caption>display area</caption>
+            <TableHead>
+              <TableRow selected>
+                <TableCell>Rank</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>{pointsNotLevel ? "Points" : "Level"}</TableCell>
+                <TableCell>Vocation</TableCell>
+                <TableCell>Server</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
+            </TableHead>
+            <TableBody>
+              {dataToDisplay.slice(0, 300).map((data, i) => (
+                <TableRow key={`${data.name}_${data.level}_${i}`} hover>
+                  <TableCell>
+                    <b>{i + 1}</b>
+                  </TableCell>
+                  <TableCell>
+                    <Link
+                      href={`https://www.tibia.com/community/?subtopic=characters&name=${encodeURIComponent(
+                        data.name
+                      )}`}
+                      target="_blank"
+                      color="secondary"
+                    >
+                      {data.name}
+                    </Link>
+                  </TableCell>
+                  <TableCell>
+                    {pointsNotLevel ? data.points : data.level}
+                  </TableCell>
+                  <TableCell>{data.voc}</TableCell>
+                  <TableCell>{data.server}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </MaterialUiTheme>
   );
 };
 export default Index;
