72c5dc3cfae487c15d983fa789c6c71154c12616
auto200 <michal.warac@gmail.com>
2020-05-07 09:18:14 +0200

    Fix dark theme

--- theme.js
+++ theme.js
@@ -1,9 +1,9 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import { red } from "@material-ui/core/colors";
+// import { red } from "@material-ui/core/colors";
 
 // Create a theme instance.
 const theme = createMuiTheme({
-  palette: "dark",
+  palette: { type: "dark" },
 });
 // const theme = createMuiTheme({
 //   palette: {
